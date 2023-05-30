4f8a833d4010889b35af5578e547b05e70e4f2d8
Bensam Mwaniki <65493075+bensammwaniki@users.noreply.github.com>
2023-05-30 13:46:14 +0300

    :building_construction: Scarfolding
    Creating a project scarfolding to start the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home/home.component';
+import { BlogsComponent } from './blog/blogs/blogs.component';
+import { ContactComponent } from './home/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
