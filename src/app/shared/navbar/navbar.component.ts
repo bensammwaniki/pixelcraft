8e487ab820f6ed308dc24c6de100d5283d04d4c9
Bensam Mwaniki <65493075+bensammwaniki@users.noreply.github.com>
2023-06-02 18:46:35 +0300

    :lipstick: Large screen navigation bar
    completedlarge screen navigation bar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener,Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  constructor( private renderer: Renderer2) {}
+  isScrolled = false;
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.isScrolled = window.pageYOffset > 130;
+  }
+  hideNavbar(): void {
+    const navbartoggle = document.getElementById('navdiv');
+    this.renderer.addClass(navbartoggle, 'nav_hide');
+  }
+  openNav(): void{
+    const mySidenav = document.getElementById("navdiv");
+    this.renderer.setStyle(mySidenav, 'width', '60%');
+    this.renderer.removeClass(mySidenav, 'nav_hide');
+  }
+  closeNav(): void{
+    const sidenav = document.getElementById("navdiv");
+    if (sidenav) {
+      sidenav.style.width = "0%";
+    }
+  }
 
 }
